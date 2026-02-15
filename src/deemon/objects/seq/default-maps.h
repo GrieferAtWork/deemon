b7b471e3c0a69d0c9274bc286e3aca6fcfe1eb7e
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2026-02-15 12:45:00 +0100

    Use generic proxy object constructors

--- src/deemon/objects/seq/default-maps.h
+++ src/deemon/objects/seq/default-maps.h
@@ -48,6 +48,12 @@ typedef struct {
 	                   mu_b); /* [1..1][const] The second map of the union. */
 } MapUnion;
 
+#define MapUnion_New(obj1, obj2)                   ((DREF MapUnion *)ProxyObject2_New(&MapUnion_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapUnion_NewInherited(obj1, obj2)          ((DREF MapUnion *)ProxyObject2_NewInherited(&MapUnion_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapUnion_NewInherited1(obj1, obj2)         ((DREF MapUnion *)ProxyObject2_NewInherited1(&MapUnion_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapUnion_NewInherited2(obj1, obj2)         ((DREF MapUnion *)ProxyObject2_NewInherited2(&MapUnion_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapUnion_NewInheritedOnSuccess(obj1, obj2) ((DREF MapUnion *)ProxyObject2_NewInheritedOnSuccess(&MapUnion_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+
 typedef struct {
 	PROXY_OBJECT_HEAD2_EX(DeeObject, mui_iter,  /* [1..1][lock(mui_lock)] The current iterator. */
 	                      MapUnion,  mui_union) /* [1..1][const] The underlying union-map. */
@@ -84,6 +90,12 @@ typedef struct {
 	                   mi_keys); /* [1..1][const] Key filter of the intersection. */
 } MapIntersection;
 
+#define MapIntersection_New(obj1, obj2)                   ((DREF MapIntersection *)ProxyObject2_New(&MapIntersection_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapIntersection_NewInherited(obj1, obj2)          ((DREF MapIntersection *)ProxyObject2_NewInherited(&MapIntersection_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapIntersection_NewInherited1(obj1, obj2)         ((DREF MapIntersection *)ProxyObject2_NewInherited1(&MapIntersection_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapIntersection_NewInherited2(obj1, obj2)         ((DREF MapIntersection *)ProxyObject2_NewInherited2(&MapIntersection_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapIntersection_NewInheritedOnSuccess(obj1, obj2) ((DREF MapIntersection *)ProxyObject2_NewInheritedOnSuccess(&MapIntersection_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+
 typedef struct {
 	PROXY_OBJECT_HEAD2_EX(DeeObject,       mii_iter,       /* [1..1][const] An iterator for `mii_intersect->mi_map' */
 	                      MapIntersection, mii_intersect); /* [1..1][const] The underlying intersection-map. */
@@ -100,6 +112,12 @@ typedef struct {
 	                   md_keys); /* [1..1][const] Set of keys that should be excluded from `md_map' */
 } MapDifference;
 
+#define MapDifference_New(obj1, obj2)                   ((DREF MapDifference *)ProxyObject2_New(&MapDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapDifference_NewInherited(obj1, obj2)          ((DREF MapDifference *)ProxyObject2_NewInherited(&MapDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapDifference_NewInherited1(obj1, obj2)         ((DREF MapDifference *)ProxyObject2_NewInherited1(&MapDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapDifference_NewInherited2(obj1, obj2)         ((DREF MapDifference *)ProxyObject2_NewInherited2(&MapDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapDifference_NewInheritedOnSuccess(obj1, obj2) ((DREF MapDifference *)ProxyObject2_NewInheritedOnSuccess(&MapDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+
 typedef struct {
 	PROXY_OBJECT_HEAD2_EX(DeeObject,     mdi_iter,  /* [1..1][const] An iterator for `mdi_diff->md_map' */
 	                      MapDifference, mdi_diff); /* [1..1][const] The underlying difference-map. */
@@ -115,6 +133,12 @@ typedef struct {
 	PROXY_OBJECT_HEAD2(msd_a,  /* [1..1][const] The first map. */
 	                   msd_b); /* [1..1][const] The second map. */
 } MapSymmetricDifference;
+
+#define MapSymmetricDifference_New(obj1, obj2)                   ((DREF MapSymmetricDifference *)ProxyObject2_New(&MapSymmetricDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapSymmetricDifference_NewInherited(obj1, obj2)          ((DREF MapSymmetricDifference *)ProxyObject2_NewInherited(&MapSymmetricDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapSymmetricDifference_NewInherited1(obj1, obj2)         ((DREF MapSymmetricDifference *)ProxyObject2_NewInherited1(&MapSymmetricDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapSymmetricDifference_NewInherited2(obj1, obj2)         ((DREF MapSymmetricDifference *)ProxyObject2_NewInherited2(&MapSymmetricDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
+#define MapSymmetricDifference_NewInheritedOnSuccess(obj1, obj2) ((DREF MapSymmetricDifference *)ProxyObject2_NewInheritedOnSuccess(&MapSymmetricDifference_Type, Dee_AsObject(obj1), Dee_AsObject(obj2)))
 
 typedef struct {
 	PROXY_OBJECT_HEAD2_EX(DeeObject,              msdi_iter,     /* [1..1][lock(msdi_lock)] The current iterator. */
@@ -144,314 +168,6 @@ typedef struct {
 
 INTDEF DeeTypeObject MapSymmetricDifference_Type;
 INTDEF DeeTypeObject MapSymmetricDifferenceIterator_Type;
-
-
-
-
-/* TODO: Use generic proxy object constructors */
-#define MapUnion_New(a, b) \
-	_MapUnion_New(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapUnion *DCALL
-_MapUnion_New(DeeObject *a, DeeObject *b) {
-	DREF MapUnion *result;
-	result = DeeObject_MALLOC(MapUnion);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	Dee_Incref(b);
-	result->mu_a = a;
-	result->mu_b = b;
-	DeeObject_Init(result, &MapUnion_Type);
-	return result;
-err:
-	return NULL;
-}
-
-#define MapUnion_New_inherit_a(a, b) \
-	_MapUnion_New_inherit_a(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapUnion *DCALL
-_MapUnion_New_inherit_a(/*inherit(always)*/ DREF DeeObject *a, DeeObject *b) {
-	DREF MapUnion *result;
-	result = DeeObject_MALLOC(MapUnion);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(b);
-	result->mu_a = a; /* Inherited */
-	result->mu_b = b;
-	DeeObject_Init(result, &MapUnion_Type);
-	return result;
-err:
-	Dee_Decref(a);
-	return NULL;
-}
-
-#define MapUnion_New_inherit_b(a, b) \
-	_MapUnion_New_inherit_b(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapUnion *DCALL
-_MapUnion_New_inherit_b(DeeObject *a, /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapUnion *result;
-	result = DeeObject_MALLOC(MapUnion);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	result->mu_a = a;
-	result->mu_b = b; /* Inherited */
-	DeeObject_Init(result, &MapUnion_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	return NULL;
-}
-
-#define MapUnion_New_inherit_ab(a, b) \
-	_MapUnion_New_inherit_ab(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapUnion *DCALL
-_MapUnion_New_inherit_ab(/*inherit(always)*/ DREF DeeObject *a,
-                         /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapUnion *result;
-	result = DeeObject_MALLOC(MapUnion);
-	if unlikely(!result)
-		goto err;
-	result->mu_a = a; /* Inherited */
-	result->mu_b = b; /* Inherited */
-	DeeObject_Init(result, &MapUnion_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	Dee_Decref(a);
-	return NULL;
-}
-
-
-
-
-
-#define MapIntersection_New(a, b) \
-	_MapIntersection_New(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapIntersection *DCALL
-_MapIntersection_New(DeeObject *a, DeeObject *b) {
-	DREF MapIntersection *result;
-	result = DeeObject_MALLOC(MapIntersection);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	Dee_Incref(b);
-	result->mi_map = a;
-	result->mi_keys = b;
-	DeeObject_Init(result, &MapIntersection_Type);
-	return result;
-err:
-	return NULL;
-}
-
-#define MapIntersection_New_inherit_a(a, b) \
-	_MapIntersection_New_inherit_a(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapIntersection *DCALL
-_MapIntersection_New_inherit_a(/*inherit(always)*/ DREF DeeObject *a, DeeObject *b) {
-	DREF MapIntersection *result;
-	result = DeeObject_MALLOC(MapIntersection);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(b);
-	result->mi_map = a; /* Inherited */
-	result->mi_keys = b;
-	DeeObject_Init(result, &MapIntersection_Type);
-	return result;
-err:
-	Dee_Decref(a);
-	return NULL;
-}
-
-#define MapIntersection_New_inherit_b(a, b) \
-	_MapIntersection_New_inherit_b(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapIntersection *DCALL
-_MapIntersection_New_inherit_b(DeeObject *a, /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapIntersection *result;
-	result = DeeObject_MALLOC(MapIntersection);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	result->mi_map = a;
-	result->mi_keys = b; /* Inherited */
-	DeeObject_Init(result, &MapIntersection_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	return NULL;
-}
-
-#define MapIntersection_New_inherit_ab(a, b) \
-	_MapIntersection_New_inherit_ab(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapIntersection *DCALL
-_MapIntersection_New_inherit_ab(/*inherit(always)*/ DREF DeeObject *a,
-                                /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapIntersection *result;
-	result = DeeObject_MALLOC(MapIntersection);
-	if unlikely(!result)
-		goto err;
-	result->mi_map = a; /* Inherited */
-	result->mi_keys = b; /* Inherited */
-	DeeObject_Init(result, &MapIntersection_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	Dee_Decref(a);
-	return NULL;
-}
-
-
-
-
-
-#define MapDifference_New(a, b) \
-	_MapDifference_New(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapDifference *DCALL
-_MapDifference_New(DeeObject *a, DeeObject *b) {
-	DREF MapDifference *result;
-	result = DeeObject_MALLOC(MapDifference);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	Dee_Incref(b);
-	result->md_map = a;
-	result->md_keys = b;
-	DeeObject_Init(result, &MapDifference_Type);
-	return result;
-err:
-	return NULL;
-}
-
-#define MapDifference_New_inherit_a(a, b) \
-	_MapDifference_New_inherit_a(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapDifference *DCALL
-_MapDifference_New_inherit_a(/*inherit(always)*/ DREF DeeObject *a, DeeObject *b) {
-	DREF MapDifference *result;
-	result = DeeObject_MALLOC(MapDifference);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(b);
-	result->md_map = a; /* Inherited */
-	result->md_keys = b;
-	DeeObject_Init(result, &MapDifference_Type);
-	return result;
-err:
-	Dee_Decref(a);
-	return NULL;
-}
-
-#define MapDifference_New_inherit_b(a, b) \
-	_MapDifference_New_inherit_b(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapDifference *DCALL
-_MapDifference_New_inherit_b(DeeObject *a, /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapDifference *result;
-	result = DeeObject_MALLOC(MapDifference);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	result->md_map = a;
-	result->md_keys = b; /* Inherited */
-	DeeObject_Init(result, &MapDifference_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	return NULL;
-}
-
-#define MapDifference_New_inherit_ab(a, b) \
-	_MapDifference_New_inherit_ab(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapDifference *DCALL
-_MapDifference_New_inherit_ab(/*inherit(always)*/ DREF DeeObject *a,
-                              /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapDifference *result;
-	result = DeeObject_MALLOC(MapDifference);
-	if unlikely(!result)
-		goto err;
-	result->md_map = a; /* Inherited */
-	result->md_keys = b; /* Inherited */
-	DeeObject_Init(result, &MapDifference_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	Dee_Decref(a);
-	return NULL;
-}
-
-
-
-
-
-#define MapSymmetricDifference_New(a, b) \
-	_MapSymmetricDifference_New(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapSymmetricDifference *DCALL
-_MapSymmetricDifference_New(DeeObject *a, DeeObject *b) {
-	DREF MapSymmetricDifference *result;
-	result = DeeObject_MALLOC(MapSymmetricDifference);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	Dee_Incref(b);
-	result->msd_a = a;
-	result->msd_b = b;
-	DeeObject_Init(result, &MapSymmetricDifference_Type);
-	return result;
-err:
-	return NULL;
-}
-
-#define MapSymmetricDifference_New_inherit_a(a, b) \
-	_MapSymmetricDifference_New_inherit_a(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapSymmetricDifference *DCALL
-_MapSymmetricDifference_New_inherit_a(/*inherit(always)*/ DREF DeeObject *a, DeeObject *b) {
-	DREF MapSymmetricDifference *result;
-	result = DeeObject_MALLOC(MapSymmetricDifference);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(b);
-	result->msd_a = a; /* Inherited */
-	result->msd_b = b;
-	DeeObject_Init(result, &MapSymmetricDifference_Type);
-	return result;
-err:
-	Dee_Decref(a);
-	return NULL;
-}
-
-#define MapSymmetricDifference_New_inherit_b(a, b) \
-	_MapSymmetricDifference_New_inherit_b(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapSymmetricDifference *DCALL
-_MapSymmetricDifference_New_inherit_b(DeeObject *a, /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapSymmetricDifference *result;
-	result = DeeObject_MALLOC(MapSymmetricDifference);
-	if unlikely(!result)
-		goto err;
-	Dee_Incref(a);
-	result->msd_a = a;
-	result->msd_b = b; /* Inherited */
-	DeeObject_Init(result, &MapSymmetricDifference_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	return NULL;
-}
-
-#define MapSymmetricDifference_New_inherit_ab(a, b) \
-	_MapSymmetricDifference_New_inherit_ab(Dee_AsObject(a), Dee_AsObject(b))
-LOCAL WUNUSED NONNULL((1, 2)) DREF MapSymmetricDifference *DCALL
-_MapSymmetricDifference_New_inherit_ab(/*inherit(always)*/ DREF DeeObject *a,
-                                       /*inherit(always)*/ DREF DeeObject *b) {
-	DREF MapSymmetricDifference *result;
-	result = DeeObject_MALLOC(MapSymmetricDifference);
-	if unlikely(!result)
-		goto err;
-	result->msd_a = a; /* Inherited */
-	result->msd_b = b; /* Inherited */
-	DeeObject_Init(result, &MapSymmetricDifference_Type);
-	return result;
-err:
-	Dee_Decref(b);
-	Dee_Decref(a);
-	return NULL;
-}
 
 
 INTDEF WUNUSED NONNULL((1, 2)) int DCALL MapIntersection_NonEmpty(DeeObject *map, DeeObject *keys);
